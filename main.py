e5847fa6da2b5a531d06dea805f7bcaab1704fdd
Thom Bruce <thom@thombruce.com>
2022-12-18 01:12:53 +0000

    feat: draw @ symbol in centre of screen

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
@@ -19,7 +22,7 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
 
